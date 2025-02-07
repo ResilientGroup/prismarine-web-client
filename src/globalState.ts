9be595076074709370a465b11bef7ae2fb4b53fd
Vitaly <vital2580@icloud.com>
2025-02-07 13:01:22 +0300

    feat: Connections issues icon & reconnect button (#268)

--- src/globalState.ts
+++ src/globalState.ts
@@ -153,6 +153,8 @@ export const gameAdditionalState = proxy({
   isSneaking: false,
   isZooming: false,
   warps: [] as WorldWarp[],
+  noConnection: false,
+  poorConnection: false,
 
   usingServerResourcePack: false,
 })
