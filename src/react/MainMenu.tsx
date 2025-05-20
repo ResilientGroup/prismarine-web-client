7799ccc370f16a805e2c9515f110a9707704a500
Maksim Grigorev <gmi1987@gmail.com>
2025-05-20 19:08:34 +0300

    Update src/react/MainMenu.tsx
    Co-authored-by: Vitaly <vital2580@icloud.com>

--- src/react/MainMenu.tsx
+++ src/react/MainMenu.tsx
@@ -77,7 +77,7 @@ export default ({
     }
 
     return appConfig?.splashTextFallback || ''
-  }, [appConfig])
+  }, [])
 
   useEffect(() => {
     const configSplashFromApp = appConfig?.splashText
